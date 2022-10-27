5ebb2fa0616418db487a178cabf0eaeee6878079
Kristy Codes <17714012+KLynnM315@users.noreply.github.com>
2022-10-26 23:15:09 -0400

    Added the HomeScreen.js back in

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
 const ComponentsScreen = () => {
     const name = 'Bear Paw';
 
-    return (
-        <View>
-            <Text style={StyleSheet.textStyle}>Getting started with React Native!</Text>
-            <Text style={StyleSheet.textStyle2}>My name is {name}</Text>
-        </View>
-    );
+    return <Text style={styles.textStyle}>My name is {name}</Text>;
+   
 };
 
 const styles = StyleSheet.create({
+    
     textStyle: {
         fontSize: 45
-    },
-    textStyle2: {
-        fontSize: 20
     }
+
 });
 
 export default ComponentsScreen;
